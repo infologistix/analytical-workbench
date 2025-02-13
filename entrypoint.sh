885505b9d91e4723fba732b9c7029d92f2ea2de9
Christoph  Seidel <christophseidel@MacBook-Air-von-Christoph.local>
2025-02-13 17:38:35 +0100

    Set DISPLAY environment variable for XFCE

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,9 @@ sudo -u $USERNAME code-server \
 # Start noVNC
 /opt/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 6080 &
 
+# Set DISPLAY environment variable for XFCE
+export DISPLAY=:0
+
 # Start XFCE
 sudo -u $USERNAME xfce4-session &
 
