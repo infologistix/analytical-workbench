b8a36bac765a3ed844e2588dcf093684e5cbd7e6
Christoph  Seidel <christophseidel@MacBook-Air-von-Christoph.local>
2025-02-13 10:38:04 +0100

    entrypoint updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,42 +1,28 @@
-FROM ubuntu:22.04
+#!/bin/bash
 
-ENV DEBIAN_FRONTEND=noninteractive \
-    USERNAME=developer
+# Set up user environment
+USERNAME=developer
+mkdir -p /home/$USERNAME/.vnc
+chown -R $USERNAME:$USERNAME /home/$USERNAME
 
-# Base packages
-RUN apt-get update && apt-get install -y \
-    software-properties-common \
-    sudo \
-    iputils-ping \
-    tigervnc-standalone-server \
-    xfce4 xfce4-terminal xfce4-goodies \
-    x11vnc xvfb \
-    curl libx11-6 libxkbfile1 libsecret-1-0 \
-    dbus-x11 xterm \
-    python3 python3-pip vim \
-    websockify git \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/*
+# Start code-server
+sudo -u $USERNAME code-server \
+    --bind-addr 0.0.0.0:8080 \
+    --auth none \
+    /home/$USERNAME/code &
 
-# Firefox without Snap
-RUN add-apt-repository -y ppa:mozillateam/ppa && \
-    echo "Package: *\nPin: release o=LP-PPA-mozillateam\nPin-Priority: 1001" > /etc/apt/preferences.d/mozilla-firefox && \
-    apt-get update && apt-get install -y firefox && apt-get clean
+# Start noVNC
+/opt/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 6080 &
 
-# Code-server
-RUN curl -fsSL https://code-server.dev/install.sh | sh
+# Start XFCE
+sudo -u $USERNAME xfce4-session &
 
-# noVNC
-RUN git clone https://github.com/novnc/noVNC.git /opt/novnc \
-    && ln -s /opt/novnc/vnc_lite.html /opt/novnc/index.html
+# Start VNC server
+sudo -u $USERNAME x11vnc -display :0 \
+    -forever \
+    -shared \
+    -rfbauth /home/$USERNAME/.vnc/passwd \
+    -rfbport 5900
 
-# User setup
-RUN useradd -m -u 1000 -s /bin/bash ${USERNAME} \
-    && echo "${USERNAME}:changeme" | chpasswd \
-    && echo "${USERNAME} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${USERNAME} \
-    && chmod 440 /etc/sudoers.d/${USERNAME}
-
-COPY entrypoint.sh /
-RUN chmod +x /entrypoint.sh
-
-ENTRYPOINT ["/entrypoint.sh"]
+# Keep container running
+tail -f /dev/null
