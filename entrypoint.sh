236325a32dfe4092d74962778f83a36bb8a817b4
Christoph  Seidel <christophseidel@MacBook-Air-von-Christoph.local>
2025-02-14 14:56:37 +0100

    Wait for Xvfb

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ mkdir -p /home/$USERNAME/.vnc
 chown -R $USERNAME:$USERNAME /home/$USERNAME
 
 # Start code-server
-sudo -u $USERNAME code-server --bind-addr 0.0.0.0:8080 --auth none /home/$USERNAME/code &
+code-server --bind-addr 0.0.0.0:8080 --auth none /home/$USERNAME/code &
 
 # Start noVNC
 /opt/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 6080 &
@@ -14,18 +14,16 @@ sudo -u $USERNAME code-server --bind-addr 0.0.0.0:8080 --auth none /home/$USERNA
 # Set DISPLAY environment variable for XFCE
 # Wait for Xvfb
 export DISPLAY=:1
-Xvfb $DISPLAY -screen 0 1920x1080x24 +extension GLX +render -noreset &
-while ! xdpyinfo -display $DISPLAY >/dev/null 2>&1; do
-    sleep 0.5
-done
+Xvfb $DISPLAY -screen 0 1920x1080x24 &
+until xdpyinfo -display $DISPLAY >/dev/null 2>&1; do sleep 1; done
 
 # Start XFCE
-sudo -u $USERNAME xfce4-session &
+dbus-launch xfce4-session &
 
 sleep 3
 
 # Start VNC server
-sudo -u $USERNAME x11vnc -display $DISPLAY -forever -shared -rfbauth /home/$USERNAME/.vnc/passwd -rfbport 5900 \
+x11vnc -display $DISPLAY -forever -shared -rfbauth /home/$USERNAME/.vnc/passwd -rfbport 5900 \
     -bg \
     -noxdamage \
     -logfile /var/log/x11vnc.log
